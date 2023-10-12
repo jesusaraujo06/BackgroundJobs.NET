924b493a6db7f9e100570f676ad013a73b0ff1af
Jesús Araujo <jesusaraujo.0699@gmail.com>
2023-10-11 19:39:25 -0500

    Agregar archivos de proyecto.

---
+++ BackgroundJobs.Client/DapperDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CustomerSearch.Data;
+
+public class DapperDbContext
+{
+    private readonly IConfiguration Configuration;
+    private readonly string? connectionString;
+    public DapperDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+        connectionString = Configuration.GetConnectionString("DefaultConnection");
+    }
+
+    public IDbConnection CreateConnection() => new SqlConnection(connectionString);
+}
---
+++ BackgroundJobs.Client/Program.cs
@@ -0,0 +1,56 @@
+using CustomerSearch.Data;
+using Hangfire;
+using Hangfire.SqlServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<DapperDbContext>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddHangfire(configuration => configuration
+        .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseSqlServerStorage(connectionString, new SqlServerStorageOptions
+        {
+            SchemaName = "Jobs01",
+            CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+            SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+            QueuePollInterval = TimeSpan.Zero,
+            UseRecommendedIsolationLevel = true,
+            DisableGlobalLocks = true
+        }));
+
+builder.Services.AddHangfireServer();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHangfireDashboard();
+app.MapHangfireDashboard("/jobs", new DashboardOptions()
+{
+    DashboardTitle = "Everest Jobs",
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
