bb181ec5a0beda38244f8d00f8aa32c5590cffd8
Jesús Araujo <jesusaraujo.0699@gmail.com>
2023-10-12 11:07:02 -0500

    Crear edpoints para crear jobs

---
+++ BackgroundJobs.Client/Controllers/JobsController.cs
@@ -0,0 +1,43 @@
+using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackgroundJobs.Client.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JobsController : ControllerBase
+    {
+        private readonly IBackgroundJobClient BackgroundJobClient;
+
+        private readonly ILogger<JobsController> _logger;
+
+        public JobsController(ILogger<JobsController> logger, IBackgroundJobClient backgroundJobClient)
+        {
+            _logger = logger;
+            BackgroundJobClient = backgroundJobClient;
+        }
+
+
+        [HttpGet("CreateEnqueue")]
+        public ActionResult Enqueue()
+        {
+            BackgroundJobClient.Enqueue(() => Console.WriteLine("Tarea enqueue ejecutada"));
+            return Ok();
+        }
+
+        [HttpGet("CreateSchedule")]
+        public ActionResult Schedule()
+        {
+            BackgroundJobClient.Schedule(() => Console.WriteLine("Tarea schedule ejecutada"), TimeSpan.FromSeconds(5));
+            return Ok();
+        }
+
+        [HttpGet("CreateRecurring")]
+        public ActionResult Recurring()
+        {
+            RecurringJob.AddOrUpdate("Tarea 2", () => Console.WriteLine("Tarea reucurring ejecutada"), Cron.Minutely);
+            return Ok();
+        }
+
+    }
+}
